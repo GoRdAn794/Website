ce18512fc38e003dd4752064ea14ae8363af788d
GoRdAn794 <74813788+GoRdAn794@users.noreply.github.com>
2020-12-11 11:27:45 +0530

    Add files via upload

--- web.js
+++ web.js
@@ -10,6 +10,7 @@ $(document).ready(function(){
   var a=$('.slide').length;
   	var b=$('.slides');
   	var c=1;
+  	$("#c1").css("color","red");
   	interval = setInterval(function(){
   		b.animate({
   			marginLeft:'-=98.1vw'
@@ -59,35 +60,36 @@ $(document).ready(function(){
   	$("#ffa1").on('click',function(){
   		$('.slides').css('float','right')
 		$('.slides').animate({
-  			marginRight:'-=97vw'
-  		},1500,function(){
+  			marginRight:'-=98.1vw'
+  		},800,function(){
+
   			c++;
   			if(c==1)
   			{
   				$("#c2").css("color","white");
-  				$("#c1").css("color","red");
+  				$("#c1").css("color","white");
   				$("#c3").css("color","white");
-  				$("#c4").css("color","white");
+  				$("#c4").css("color","red");
   			}
   			else if(c==2)
   			{
-  				$("#c2").css("color","red");
+  				$("#c2").css("color","white");
   				$("#c1").css("color","white");
-  				$("#c3").css("color","white");
+  				$("#c3").css("color","red");
   				$("#c4").css("color","white");
   			}
   			else if(c==3)
   			{
   				$("#c1").css("color","white");
-  				$("#c2").css("color","white");
-  				$("#c3").css("color","red");
+  				$("#c2").css("color","red");
+  				$("#c3").css("color","white");
   				$("#c4").css("color","white");
   			}
   			else if(c==4)
   			{
-  				$("#c1").css("color","white");
+  				$("#c1").css("color","red");
   				$("#c2").css("color","white");
-  				$("#c4").css("color","red");
+  				$("#c4").css("color","white");
   				$("#c3").css("color","white");
   			}
   			if(c===a)
@@ -99,14 +101,17 @@ $(document).ready(function(){
   				b.css('marginLeft','0px');
   				c=1;
   			}
+
+
   		});
 
 	});
   	// console.log(a);
   	$("#ffa2").on('click',function(){
+  		$('.slides').css('float','left')
 		$('.slides').animate({
   			marginLeft:'-=98.1vw'
-  		},1500,function(){
+  		},800,function(){
   			c++;
   			if(c==1)
   			{
@@ -151,33 +156,33 @@ $(document).ready(function(){
 	$("#c1").on('click',function(){
 		$('.slides').css('marginLeft','0vw');
 		$("#c1").css("color","red");
-  		$("#c2").css("color","white");
-  		$("#c3").css("color","white");
-  		$("#c4").css("color","white");
+  				$("#c2").css("color","white");
+  				$("#c4").css("color","white");
+  				$("#c3").css("color","white");
 
 	});
 	$("#c2").on('click',function(){
-		$('.slides').css('margin-left','-98vw');
+		$('.slides').css('margin-left','-98.1vw');
 		$("#c1").css("color","white");
-  		$("#c2").css("color","red");
-  		$("#c3").css("color","white");
-  		$("#c4").css("color","white");
+  				$("#c2").css("color","red");
+  				$("#c4").css("color","white");
+  				$("#c3").css("color","white");
 
 	});
 	$("#c3").on('click',function(){
-		$('.slides').css('margin-left','-196.1vw');
+		$('.slides').css('margin-left','-196.20vw');
 		$("#c1").css("color","white");
-  		$("#c2").css("color","white");
-  		$("#c3").css("color","red");
-  		$("#c4").css("color","white");
+  				$("#c2").css("color","white");
+  				$("#c4").css("color","white");
+  				$("#c3").css("color","red");
 
 	});
 	$("#c4").on('click',function(){
-		$('.slides').css('margin-left','-392.2vw');
+		$('.slides').css('margin-left','-392.40vw');
 		$("#c1").css("color","white");
-  		$("#c2").css("color","white");
-  		$("#c3").css("color","white");
-  		$("#c4").css("color","red");
+  				$("#c2").css("color","white");
+  				$("#c4").css("color","red");
+  				$("#c3").css("color","white");
 
 	})
 
